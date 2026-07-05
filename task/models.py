f45a6fc46097da72f10a254234071700ddf13037
KristiyanYanakiev <yankris_92@yahoo.com>
2026-07-05 17:42:08 +0300

    more solutions

--- task/models.py
+++ task/models.py
@@ -1,11 +1,33 @@
+from datetime import timedelta
+
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
 from task.choices import Status
 
 UserModel = get_user_model()
-# Create your models here.
+
+
+
+class TaskManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(owner=user)
+
+    def grouped_by_status(self, user):
+        tasks = self.for_user(user)
+        return {
+            'pending': tasks.filter(status=Status.PENDING),
+            'in_progress': tasks.filter(status=Status.IN_PROGRESS),
+            'completed': tasks.filter(
+                status=Status.COMPLETED,
+                updated_at__gte=timezone.now() - timedelta(days=7)
+            )
+        }
+
+
 class Task(models.Model):
+    objects = TaskManager()
     title = models.CharField(max_length=100)
     notes = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
